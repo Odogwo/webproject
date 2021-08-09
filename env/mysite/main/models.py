695dbb4f323777791fb502805841e5b0a2c8a1f9
Martins Nkume <martins.nkume@gmail.com>
2021-08-09 23:36:58 +0100

    added product model, instll pillow, configure media and adminsite

--- env/mysite/main/models.py
+++ env/mysite/main/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
 class Product(models.Model):
-	product_name = models.CharField(max_length=150)
+	product_name = models.CharField(max_length=150, null=True)
+	product_type = models.CharField(max_length=25,null=True)
+	product_description = models.TextField(default="No Description")
+	affiliate_url = models.SlugField(blank=True, null=True)
+	product_image = models.ImageField(upload_to='images/', null=True)
+
+	def __str__(self):
+		return self.product_name
--- env/mysite/mysite/urls.py
+++ env/mysite/mysite/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings #add this
+from django.conf.urls.static import static #add this
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ('main.urls')),   #add this
 ]
+
+if settings.DEBUG: #add this
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
