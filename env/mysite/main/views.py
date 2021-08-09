ddff4cbf6c1062bd4fc4f6a8db8ecba78fa576a4
Martins Nkume <martins.nkume@gmail.com>
2021-08-10 00:02:47 +0100

    resize image, added product, home

--- env/mysite/main/views.py
+++ env/mysite/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Product #import Product from models
 
 # Create your views here.
 def homepage(request):
-	return render(request = request, template_name="main/home.html"	)
+	product = Product.objects.all() #queryset containing all products we just created
+	return render(request=request, template_name="main/home.html", context={'product':product})
