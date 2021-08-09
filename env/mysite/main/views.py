73c9354d4217a1bb07b9bea9aade2588e8a4bbac
Martins Nkume <martins.nkume@gmail.com>
2021-08-10 00:07:03 +0100

    limit numbrer of pages to dispolayin views

--- env/mysite/main/views.py
+++ env/mysite/main/views.py
@@ -3,5 +3,5 @@ from .models import Product #import Product from models
 
 # Create your views here.
 def homepage(request):
-	product = Product.objects.all() #queryset containing all products we just created
+	product = Product.objects.all()[:4] #queryset containing all products we just created
 	return render(request=request, template_name="main/home.html", context={'product':product})
