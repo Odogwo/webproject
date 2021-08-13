daebd87adee9335e9a41f272383e8af6c1600d1d
Martins Nkume <martins.nkume@gmail.com>
2021-08-13 01:18:45 +0100

    added template/base.html, updated the view

--- env/bookr/reviews/views.py
+++ env/bookr/reviews/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+
 
 
 def index(request):
-	name = request.GET.get("name") or "world"
-	return HttpResponse("Hello, {}!".format(name))
+	name = "world"
+	return render(request, "base.html", {"name": name})
+
 
 
