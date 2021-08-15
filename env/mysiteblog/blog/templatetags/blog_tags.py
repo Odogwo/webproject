4857210727a7f6f4d3bffceb9b11c76e09fb43ee
Martins Nkume <martins.nkume@gmail.com>
2021-08-15 23:37:20 +0100

    added markdown

--- env/mysiteblog/blog/templatetags/blog_tags.py
+++ env/mysiteblog/blog/templatetags/blog_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 
 register = template.Library()
@@ -20,6 +22,10 @@ def get_most_commented_posts(count=5):
            ).order_by('-total_comments')[:count]
 
 
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
 
 
 
