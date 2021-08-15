8fb54f9192d46be6c91830b33de2d3c84fd92cad
Martins Nkume <martins.nkume@gmail.com>
2021-08-15 12:54:09 +0100

    blogmysite: models, details,static,templaTES

--- env/mysiteblog/blog/models.py
+++ env/mysiteblog/blog/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset()\
+                          .filter(status='published')
+
+
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -23,5 +36,19 @@ class Post(models.Model):
         ordering = ('-publish',)
     def __str__(self):
         return self.title
+
+
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
+
+
+
 
 
