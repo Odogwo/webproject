aaa9b8aa67696165301b2438685e58fcfb2b29ba
Martins Chukwuemeka Nkume <42843801+Odogwo@users.noreply.github.com>
2023-12-08 13:24:24 +0100

    Update ask_edit2.py
    esc terminate program

--- html-css-Js/ask_edit2.py
+++ html-css-Js/ask_edit2.py
@@ -1,31 +1,43 @@
 import toml
+import keyboard
 
 def print_section(section):
     print(f"Section: {section['name']}\n")
     print(section['content'])
     print("\n" + "-" * 40 + "\n")
 
+def wait_for_enter_or_escape():
+    while True:
+        if keyboard.is_pressed('enter'):
+            return True
+        elif keyboard.is_pressed('esc'):
+            return False
+
 def main():
     # Load the TOML data
-    with open('introduction.toml', 'r') as file:
+    with open('your_file_path_here.toml', 'r') as file:
         data = toml.load(file)
 
     # Print sections
     for section in data['sections']:
-        input("Press Enter to load the next section...")
-        print()
+        print("Press Enter to load the next section. Press ESC to terminate.")
+        if not wait_for_enter_or_escape():
+            return
         print_section(section)
 
     # Print relationships
     for relationship in data['relationships']:
-        input("Press Enter to load the next relationship...")
+        print("Press Enter to load the next relationship. Press ESC to terminate.")
+        if not wait_for_enter_or_escape():
+            return
         print(f"Relationship: {relationship['name']}\n")
         print(relationship['description'])
         for section_name in relationship['sections']:
             section = next((s for s in data['sections'] if s['name'] == section_name), None)
             if section:
-                input("Press Enter to load the next section...")
-                print()
+                print("Press Enter to load the next section. Press ESC to terminate.")
+                if not wait_for_enter_or_escape():
+                    return
                 print_section(section)
         print("\n" + "=" * 40 + "\n")
 
